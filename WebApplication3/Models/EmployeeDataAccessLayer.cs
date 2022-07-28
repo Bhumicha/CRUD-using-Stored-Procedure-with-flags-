db8292861803a9b3c462f839f5baccb5e2598eb4
sbhum <sbhum@DESKTOP-BCO73O8>
2022-07-28 09:38:32 +0545

    Add project files.

---
+++ WebApplication3/Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using WebApplication3.Models;
+
+
+namespace WebApplication3.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+        string connectionString = "Data Source=DESKTOP-BCO73O8\\SQLEXPRESS;Initial Catalog=Office;Integrated Security=True";
+
+        //To View all employees details    
+        public IEnumerable<Employee> GetAllEmployees()
+        {  
+            List<Employee> lstemployee = new List<Employee>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spforemployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", null);
+                cmd.Parameters.AddWithValue("@Gender", null);
+                cmd.Parameters.AddWithValue("@Department", null);
+                cmd.Parameters.AddWithValue("@City", null);
+                cmd.Parameters.AddWithValue("@flag", "Select");
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+
+                    employee.ID = Convert.ToInt32(rdr["EmployeeID"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.Department = rdr["Department"].ToString();
+                    employee.City = rdr["City"].ToString();
+                    cmd.Parameters.AddWithValue("@flag", "Select");
+
+                    lstemployee.Add(employee);
+                }
+                con.Close();
+            }
+            return lstemployee;
+        }
+        //To Add new employee record    
+        public void AddEmployee(Employee employee)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spforemployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                cmd.Parameters.AddWithValue("@Department", employee.Department);
+                cmd.Parameters.AddWithValue("@City", employee.City);
+                cmd.Parameters.AddWithValue("@flag", "Insert");
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        //To Update the records of a particluar employee  
+        public void UpdateEmployee(Employee employee)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spforemployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpId", employee.ID);
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                cmd.Parameters.AddWithValue("@Department", employee.Department);
+                cmd.Parameters.AddWithValue("@City", employee.City);
+                cmd.Parameters.AddWithValue("@flag", "update");
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        //Get the details of a particular employee  
+        public Employee GetEmployeeData(int? id)
+        {
+            Employee employee = new Employee();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * FROM tblEmployee WHERE EmployeeID= " + id;
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    employee.ID = Convert.ToInt32(rdr["EmployeeID"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.Department = rdr["Department"].ToString();
+                    employee.City = rdr["City"].ToString();
+                }
+            }
+            return employee;
+        }
+
+        //To Delete the record on a particular employee  
+        public void DeleteEmployee(int? id)
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spforemployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpId", id);
+                cmd.Parameters.AddWithValue("@flag", "delete");
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
+
